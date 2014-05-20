c06df22aa8fd388dfc875eddb6bd25b4e360cfcd
wangyang <igiuify@gmail.com>
2014-05-20 10:15:58 +0800

    修正使用步骤，添加关于服务名选择的说明

--- AutoProxySwitch.sh
+++ AutoProxySwitch.sh
@@ -8,11 +8,13 @@
 ###
 
 yourProxy="xxxxxxx"
+# 从“系统偏好设置->网络设置”中可以看到首选的网络，一般常用的服务名有：Wi-Fi，Airport 和 Ethernet
+netPort="Wi-Fi"
 if [[ `scutil --proxy | grep ProxyAutoConfigEnable | awk '{ print $3 }'` == "1" ]]; then
-    networksetup -setautoproxyurl Wi-Fi " "
-    networksetup -setautoproxystate Wi-Fi off
+    networksetup -setautoproxyurl $netPort " "
+    networksetup -setautoproxystate $netPort off
 	osascript -e 'display notification "已关闭" with title "自动代理服务"'
 else
-    networksetup -setautoproxyurl Wi-Fi $yourProxy
+    networksetup -setautoproxyurl $netPort $yourProxy
 	osascript -e 'display notification "已开启" with title "自动代理服务"'
 fi
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ AutoProxySwith
 
 ## Install
 1. 配置`AutoProxySwith.sh`里的`yourProxy`变量为你的自动代理地址。
+2. 配置`AutoProxySwith.sh`里的`netPort`变量为你的首选网络服务，常见的网络服务在代码理有注释。
 2. 打开终端执行`sudo chmod u+s /usr/sbin/networksetup`，为networksetup永久提升权限。因为上面的script用到了networksetup，这个工具在执行特定操作时需要提升权限。
 3. 使用Automator创建一个Run Shell Script应用，script内容为`AutoProxySwith.sh`里的内容
 4. 现在导出app应用，放到你喜欢的地方。使用spotlight搜索关键字“AutoProxySwitch”，即可找到该应用
